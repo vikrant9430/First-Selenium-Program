1cd330ec795779db4bb58accb9912c4c6efa59ec
Vikrant Nandan <vikrant1089@gmail.com>
2018-02-06 22:46:32 +0530

    First Commit

---
+++ src/com/selenium/first/FirstProgram.java
@@ -0,0 +1,16 @@
+package com.selenium.first;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!!");
+		openbrowser();
+	}
+	
+	public static void openbrowser(){
+		System.out.println("Hello Selenium!!");
+	}
+
+}
